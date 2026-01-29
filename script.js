0d6dc17ad80037aaa100c25dfd94157dc7ada777
ibn-azam <farhan.tsx@gmail.com>
2026-01-29 08:21:31 +0000

    initialized git

---
+++ script.js
@@ -0,0 +1,61 @@
+
+// This is variable for Numeric
+var month = 25;
+var year = 2001;
+var date = 14/11/2001;
+var rate = 1250;
+var discount = 250;
+console.log(month);
+console.log(year);
+console.log(rate);
+
+// this is variable for string 
+var name ="Farhan Khan";
+var address ="amborkhana sylhet";
+var country ="Bangladesh";
+console.log(name);
+console.log(country);
+console.log(typeof address);
+console.log(address);
+
+// This is variable for boolean 
+var isRich = false;
+var passed = true;
+var failed = false;
+console.log(passed);
+console.log(typeof failed);
+
+// Practice Task 
+var total = 1000;
+var orangePrize = 350;
+var applePrize = 350;
+var totalCost =(orangePrize + applePrize);
+var storeName = "Farhan Store";
+var isReturned = true;
+console.log(total - totalCost);
+console.log(typeof totalCost);
+console.log(typeof isReturned);
+console.log(typeof storeName);
+
+var name = "Farhan Khan";
+var birthYear = 2001;
+var deathYear = 2048;
+var age =(deathYear - birthYear);
+console.log(name);
+console.log(typeof name);
+console.log(typeof birthYear);
+console.log(typeof deathYear);
+console.log(age);
+
+var name = "Motin Treaders";
+var rice = 35;
+rice *= 3;
+console.log(rice);
+
+var milk = 90;
+milk /= 2;
+console.log(milk);
+
+var airConditioner = 65000;
+airConditioner -= 5000;
+console.log(airConditioner);
